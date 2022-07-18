88f01ec54e45847ed956db82ae21d4f70772a0ff
Kkkkkkkus <108563939+Kkkkkkkus@users.noreply.github.com>
2022-07-18 15:18:51 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -45,12 +45,12 @@ await sleep(5000)
 console.clear() 
 console.log(color(`█████████████████████████\n█▄─▄▄▀█▄─▄▄─█▄─▄▄─█▄─▄▄─█\n██─██─██─▄█▀██─▄████─▄███\n▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▀▀▀▄▄▄▀▀▀`)) 
 const users = [
-		{Creator: 'DEFF', Whatsapp: 6289501060783, NameBot: global.namebot, Versi: 7 }
+		{Creator: 'LuckXxy', Whatsapp: 6285904142544, NameBot: global.namebot, Versi: 7 }
 		];
 		console.table(users)
 
 
-sock.browserDescription = ["DEFF-MD", "Mac", "3.0.0"];
+sock.browserDescription = ["DarkStalker BOT", "Mac", "3.0.0"];
 sock.ev.on('messages.upsert', async chatUpdate => {
 try {
 mek = chatUpdate.messages[0]
